36c637827b73968d8aae1a4e6b3912da32ddb7f7
HauntedHunteer <m.niemczuk@interia.pl>
2021-11-29 11:05:30 +0100

    refactor

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -1,12 +1,10 @@
 class Route {
-  final String routeId;
   final String airlineCode;
   final String fromIata;
   final String toIata;
 
   Route(
       {
-        required this.routeId,
         required this.airlineCode,
         required this.fromIata,
         required this.toIata
