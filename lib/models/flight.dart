379577206720e744730bf536eb0dbd43af4d6f16
HauntedHunteer <m.niemczuk@interia.pl>
2021-12-06 13:42:53 +0100

    searched_F flight model updated

--- lib/models/flight.dart
+++ lib/models/flight.dart
@@ -5,6 +5,10 @@ class Flight {
   final String flightCode;
   final String fromIata;
   final String toIata;
+  final String fromCity;
+  final String toCity;
+  final String fromCountry;
+  final String toCountry;
 
   Flight(
       {required this.capacity,
@@ -12,7 +16,12 @@ class Flight {
       required this.time,
       required this.flightCode,
       required this.fromIata,
-      required this.toIata});
+      required this.toIata,
+      required this.fromCity,
+      required this.toCity,
+      required this.fromCountry,
+      required this.toCountry
+      });
 
   factory Flight.fromMap(Map<String, dynamic> map) {
     return Flight(
@@ -22,6 +31,10 @@ class Flight {
       fromIata: map['from_iata'],
       toIata: map['to_iata'],
       time: map['time'],
+      fromCity: map['from_city'],
+      toCity: map['to_city'],
+      fromCountry: map['from_country'],
+      toCountry: map['to_country']
     );
   }
 
@@ -32,7 +45,11 @@ class Flight {
       'flight_code': flightCode,
       'from_iata': fromIata,
       'to_iata': toIata,
-      'time': time
+      'time': time,
+      'from_city': fromCity,
+      'to_city': toCity,
+      'from_country': fromCountry,
+      'to_country': toCountry
     };
   }
 }
